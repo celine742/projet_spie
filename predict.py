f4fab694129573397844ea769ab488339c9e9fa7
nicolas29200 <nicolas.quere3@gmail.com>
2023-01-19 23:17:35 +0100

    predict

--- predict.py
+++ predict.py
@@ -2,20 +2,21 @@ from fastapi import APIRouter
 import pandas as pd
 import os
 import datetime
-
+from prophet import Prophet
+from prophet.serialize import model_to_json, model_from_json
+import datetime as dt
 router = APIRouter()
 
 @router.post("/api/predict")
-async def prediction_consommation(date: str, heure: str, type_habitation: str, surface: int, nb_habitants: int):
-    menage = {
-        'Date': [date],
-        'Heure': [heure],
-        'Maison ou appartement': [type_habitation],
-        'Surface de l\'habitation': [surface],
-        'Nombre d\'habitants': [nb_habitants]
-    }
-    df = pd.DataFrame(menage, index =[0])
-    formatted_date = datetime.datetime.strptime(date, '%m/%d/%Y')
-    jour_suivant = (formatted_date + datetime.timedelta(days=1)).date()
-    conso = 0
-    return f"La consommation prédite le {jour_suivant} à {heure} pour un(e) {type_habitation} de {surface}m² habité(e) par {nb_habitants} habitants est de {conso}kWh."
+async def prediction_consommation(année: int,mois:int,jour:int, heure: str, type_habitation: str, surface: int, nb_habitants: int):
+    with open(f'Model.data {type_habitation}{surface}-{nb_habitants}.json', 'r') as f:
+        model = model_from_json(f.read())  # Load model
+    dateTest=dt.date(année,mois,jour)
+    dateWithHour=dt.datetime.combine(dateTest, dt.time(15,30))+dt.timedelta(days=1)
+    Datepredict=[]
+    Datepredict.append(dateWithHour)
+    testDates = pd.DataFrame(Datepredict, columns=['ds'])
+    #formatted_date = datetime.datetime.strptime(date, '%m/%d/%Y')
+    #jour_suivant = (formatted_date + datetime.timedelta(days=1)).date()
+    forecast = model.predict(testDates)
+    return f"La consommation prédite le {dateWithHour} pour un(e) {type_habitation} de {surface}m² habité(e) par {nb_habitants} habitants est de {forecast.yhat[0]}kWh."
